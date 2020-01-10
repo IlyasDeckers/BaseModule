f730163839fb7fb579535c6da113f7b5f5df2cfa
IlyasDeckers <deckersilyas@gmail.com>
2020-01-10 15:54:47 +0100

    implement BaseQueryBuilder

--- src/BaseRepository.php
+++ src/BaseRepository.php
@@ -4,12 +4,14 @@ namespace Clockwork\Base;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Clockwork\Base\Traits\Transaction;
 use Illuminate\Http\Request;
 
-abstract class BaseRepository implements BaseRepositoryInterface
+abstract class BaseRepository extends BaseQueryBuilder implements BaseRepositoryInterface
 {
-    protected object $request;
+    protected $model;
+
+    // Deprecated??
+    protected Request $request;
 
     abstract public function store(Request $request) : object;
 
@@ -26,7 +28,6 @@ abstract class BaseRepository implements BaseRepositoryInterface
     public function find(Request $request) : object
     {
         return $this->itemResponse(
-            $request,
             $this->model->where('id', $request->id)
         );
     }
@@ -40,138 +41,7 @@ abstract class BaseRepository implements BaseRepositoryInterface
     public function getAll(Request $request) : Collection  
     {   
         return $this->collectionResponse(
-            $request,
             $this->model
         );
-    }
-
-    /**
-     * Get an item and it's relations
-     *
-     * @param object $request
-     * @param [type] $query
-     * @return collection
-     */
-    public function itemResponse(Request $request, object $query) : object
-    {
-        $this->request = $request;
-
-        return $query
-            ->when($request->has('with'), [$this, 'with'])
-            ->first();
-    }
-
-    /**
-     * Get a collection with relations,
-     * scopes, sorting and filtering.
-     *
-     * @param object $request
-     * @param object $query
-     * @return collection
-     */
-    public function collectionResponse(Request $request, object $query) : Collection
-    {
-        $this->request = $request;
-
-        $query = $query
-            ->when($request->has('filter'), [$this, 'filter'])
-            ->when($request->has('with'), [$this, 'with'])
-            ->when($request->has('scopes'), [$this, 'scopes'])
-            ->when(
-                $request->has('sort') && $request->sort !== null,
-                /* if */   [$this, 'sort'],
-                /* else */ [$this, 'noSort']
-            );
-
-        if ($request->paginate == 'true') {
-            return $query->paginate($request->per_page);
-        }
-
-        return $query->get()
-            ->when($request->has('groupBy'), [$this, 'groupBy']);
-    }
-
-    /**
-     * Filter a query
-     *
-     * @param Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function filter(Builder $query) : Builder
-    {
-        return $query->search($this->request->filter);
-    }
-
-    /**
-     * Load the relationships
-     *
-     * @param Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function with(Builder $query) : Builder
-    {
-        return $query->with(
-            explode(',', $this->request->with)
-        );
-    }
-
-    /**
-     * Get the query scopes
-     *
-     * @param Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopes(Builder $query) : Builder
-    {
-        $decoded = json_decode($this->request->scopes, true);
-
-        if (json_last_error() == JSON_ERROR_NONE) {
-            dd($decoded);
-            $scopes = $decoded;
-        } elseif (!is_object($decoded) && !is_array($decoded)) {
-            $scopes = explode(',', $this->request->scopes);
-        } else {
-            $scopes = [];
-        }
-
-        return $query->scopes($scopes);
-    }
-
-    /**
-     * Sort the query
-     *
-     * @param Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function sort(Builder $query) : Builder
-    {
-        foreach (explode(',', $this->request->sort) as $sort) {
-            list($sortCol, $sortDir) = explode('|', $sort);
-            $query = $query->orderBy($sortCol, $sortDir);
-        }
-
-        return $query;
-    }
-
-    /**
-     * Default sort method
-     *
-     * @param Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function noSort(Builder $query) : Builder
-    {
-        return $query->orderBy('id', 'asc');
-    }
-
-    /**
-     * GroupBy
-     * 
-     * @param Builder $query
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function groupBy(Builder $query) : Builder
-    {
-        return $query->groupBy($this->request->groupBy);
     }
 }
